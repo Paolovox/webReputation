fe406bc85641f25436801974277630971649656b
Paolovox <paolomondillo@gmail.com>
2018-09-05 16:55:08 +0200

    fx

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,10 +13,5 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(RoleTableSeeder::class);
         $this->call(UserTableSeeder::class);
-        $this->call(ClientTableSeeder::class);
-        $this->call(DossierTableSeeder::class);
-        $this->call(LinkTableSeeder::class);
-        $this->call(DocumentTableSeer::class);
-        $this->call(TicketTableSeeder::class);
     }
 }
