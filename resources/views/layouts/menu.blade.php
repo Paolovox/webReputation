dda00f2ba99ce80717e041e8e973e8e7a0e02f46
Paolovox <paolomondillo@gmail.com>
2018-09-05 16:45:49 +0200

    commit

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -1,12 +1,10 @@
     <aside id="s-main-menu" class="sidebar" style="margin-top: 10.8rem;">
         <ul class="main-menu">
-          @if(\Auth::user()->isAdmin())
             <li>
                 <a href="{{route('platforms.index')}}"><i class="zmdi zmdi-view-web zmdi-hc-fw"></i> Platforms</a>
             </li>
             <li>
                 <a href="{{route('keywords.index')}}"><i class="zmdi zmdi-info zmdi-hc-fw"></i> Keywords</a>
             </li>
-            @endif
         </ul>
     </aside>
